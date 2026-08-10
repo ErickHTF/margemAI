808f625d0f64ce3ff2dc4fb3d5f3edb40d16760c
Erick <erick.henrique4@outlook.com>
2026-08-10 19:39:24 -0300

    feat: springboot structure files

---
+++ src/main/java/com/example/margemAI/MargemAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.margemAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargemAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MargemAiApplication.class, args);
+	}
+
+}
